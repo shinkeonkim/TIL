fa182178cc37bc51302c188e4481951af8a82828
shinkeonkim <singun11@naver.com>
2020-03-16 10:19:19 +0900

    TIL: go  variable

---
+++ Golang/src/variable/variable.go
@@ -0,0 +1,15 @@
+package main
+func main() {
+	var a int = 3
+	var b int
+	var c float32 = 3.14
+	var d float32
+	var i,j,k = 1,2,"a"
+	println(a,b)
+	println(c,d)
+	println(i,j,k)
+
+// 3 0
+// +3.140000e+000 +0.000000e+000
+// 1 2 a
+}
